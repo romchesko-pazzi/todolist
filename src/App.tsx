0b784d67db283d0c2a1176c4c4b3e8287de75dc2
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-03-15 15:48:22 +0300

    refactor: minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {v1} from 'uuid';
 import './App.css';
 
-import {TaskType, Todolist} from './Todolist';
+import {Todolist} from './Todolist';
 import {AddForm} from "./components/AddForm/AddForm";
 import {AppBar, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
 import {Menu} from "@mui/icons-material";

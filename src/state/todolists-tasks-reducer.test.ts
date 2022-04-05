321ed79d256e6e8e9f1104edeb3cea51b68d3d5f
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-04-05 14:53:26 +0300

    feat: add test for IDs

---
+++ src/state/todolists-tasks-reducer.test.ts
@@ -0,0 +1,19 @@
+import {TasksStateType, TodolistType} from "../App";
+import {addTodolistAC, todolistsReducer} from "./todolistsReducer";
+import {tasksReducer} from "./tasksReducer";
+
+test("id should be similar", () => {
+    const startTasksState: TasksStateType = {};
+    const startTodolistsState: Array<TodolistType> = [];
+
+    const action = addTodolistAC("newTodolist");
+
+    const endTodolistsState = todolistsReducer(startTodolistsState, action);
+    const endTasksState = tasksReducer(startTasksState, action);
+
+    const key = Object.keys(endTasksState);
+
+    expect(key[0]).toStrictEqual(endTodolistsState[0].id);
+    expect(endTodolistsState[0].id).toStrictEqual(action.payload.todolistID)
+
+})

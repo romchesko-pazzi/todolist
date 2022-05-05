d36d630e5713ab8add0dc83faef0ed6ebd3c682a
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-05-05 20:11:40 +0300

    refactor: create Task component

---
+++ src/components/Task.tsx
@@ -0,0 +1,35 @@
+import React, {useCallback} from 'react';
+import s from "../Todolist.module.css";
+import {Checkbox, IconButton} from "@mui/material";
+import {EditableSpan} from "./EditableSpan/EditableSpan";
+import {Delete} from "@mui/icons-material";
+import {TaskType} from "../Todolist";
+
+type TaskPropsType = {
+    task: TaskType
+    deleteTask: (taskID: string) => void
+    renameTodolistTask: (newTitle: string, taskID: string) => void
+    changeTaskStatus: (todolistID: string, id: string, value: boolean) => void
+    todolistID: string
+}
+
+export const Task = React.memo((props: TaskPropsType) => {
+    console.log("TASK");
+    const {deleteTask, renameTodolistTask, changeTaskStatus, todolistID, task} = props;
+
+    const renameTaskHandler = useCallback((newTitle: string) => {
+        renameTodolistTask(newTitle, task.id);
+    }, []);
+
+    return (
+        <div className={task.isDone ? s.isDone : ""}>
+            <Checkbox checked={task.isDone}
+                      onChange={(e) => changeTaskStatus(todolistID, task.id, e.currentTarget.checked)}/>
+            <EditableSpan name={task.title}
+                          callback={renameTaskHandler}/>
+            <IconButton onClick={() => deleteTask(task.id)}>
+                <Delete/>
+            </IconButton>
+        </div>
+    );
+});

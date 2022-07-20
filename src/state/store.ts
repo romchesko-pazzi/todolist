f1d4d95773eff649e9b13f04c86d2856371afa6c
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-20 22:00:48 +0300

    add auth reducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,10 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
-import {TodolistsReducer} from "./todolistsReducer";
-import {TasksReducer} from "./tasksReducer";
+import {TodolistsReducer} from "./reducers/todolistsReducer";
+import {TasksReducer} from "./reducers/tasksReducer";
 // import {devToolsEnhancer} from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import {AppReducer} from "./appReducer";
+import {AppReducer} from "./reducers/appReducer";
+import {AuthReducer} from "./reducers/authReducer";
 
 export type RootStateType = ReturnType<typeof rootReducer>;
 
@@ -11,8 +12,12 @@ export const rootReducer = combineReducers({
     todolists: TodolistsReducer,
     tasks: TasksReducer,
     app: AppReducer,
+    auth: AuthReducer,
 })
 
 
 // devToolsEnhancer({})
 export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+//@ts-ignore
+window.store = store;

03cb01a4989f23e18e22cab6dd2a9d128fdcd1fd
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-07-20 22:01:26 +0300

    react 18 migrate

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {App} from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
+import {App} from "./app/App";
 import {store} from "./state/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
     <Provider store={store}>
         <App/>
     </Provider>,
-    document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
